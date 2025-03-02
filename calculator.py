65e1b9ee32b095147437d3f7e7b7964bba121a0d
CraniumX <pavel.vagin7777@yandex.ru>
2025-03-02 18:58:06 +0300

    2.0

--- calculator.py
+++ calculator.py
@@ -15,8 +15,12 @@ def calculator(numb_one, numb_two):
             return res
     except: ValueError
     print("Нужно выбрать один из вариантов. Попробуйте еще раз...")
+    return None
 
 numb_one = float(input("Введите первое число: "))
 numb_two = float(input("Введите второе число: "))
 
-print(calculator(numb_one, numb_two))
+result = calculator(numb_one, numb_two)
+
+if result != None:
+    print("Ответ:", result)
